6c1d8cc8255846626eb77db5c76b83271975d2bc
adrien <adrien.charrier@outlook.fr>
2023-09-13 15:53:31 +0000

    FEAT - targets monitored db

---
+++ app/Models/TargetsMonitored.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TargetsMonitored extends Model
+{
+    use HasFactory;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'protocol',
+        'path',
+        'port',
+        'interval',
+        'status',
+    ];
+}
