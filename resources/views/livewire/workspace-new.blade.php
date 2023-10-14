c916862ab3ee4c8ce9d88a0724933339f0b6f220
adrien <adrien.charrier@outlook.fr>
2023-10-14 16:57:51 +0200

    FEAT - endpoint forms style

--- resources/views/livewire/workspace-new.blade.php
+++ resources/views/livewire/workspace-new.blade.php
@@ -1,26 +1,32 @@
-<div>
-    <form wire:submit.prevent="create">
-        <div class="form-group">
-            <label for="name">{{__('Workspace title')}}</label>
+<div class="workspace-create">
+    <form wire:submit.prevent="create" class="workspace-form">
+        <div class="workspace-form__name">
+            <label for="name">{{ __('Workspace title') }}</label>
             <input type="text" id="name" wire:model="name">
-            @error('name') <span>{{ $message }}</span> @enderror
+            @error('name')
+                <span>{{ $message }}</span>
+            @enderror
         </div>
 
-        <div class="form-group">
-            <label for="description">{{__('Workspace description')}}</label>
+        <div class="workspace-form__description">
+            <label for="description">{{ __('Workspace description') }}</label>
             <input type="text" id="description" wire:model="description">
-            @error('description') <span>{{ $message }}</span> @enderror
+            @error('description')
+                <span>{{ $message }}</span>
+            @enderror
         </div>
 
         <button type="submit">
-            {{__('Create')}}
+            {{ __('Create') }}
         </button>
     </form>
 
     <script>
         document.addEventListener('livewire:initialized', () => {
             @this.on('workspace-created', (event) => {
-                @this.dispatch('notify', { message: '{{__('New workspace added successfully')}}' })
+                @this.dispatch('notify', {
+                    message: '{{ __('New workspace added successfully') }}'
+                })
             });
         });
     </script>
