061685e68a4c5acbcc57ba638b34c6a026fdb7f4
adrien <adrien.charrier@outlook.fr>
2023-10-15 23:03:00 +0200

    FIX - Update user form

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,7 +6,7 @@
             </a>
         </div>
         <div class="app-header__greeting">
-            {{ __('Hello :name!', ['name' => Auth::user()->first_name]) }}
+            {{ __('Hello :name!', ['name' => Auth::user()->display_name ?: Auth::user()->first_name]) }}
         </div>
         <div class="app-header__menu">
             @include('layouts.navigation')
