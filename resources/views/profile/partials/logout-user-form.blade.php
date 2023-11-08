13ff5f612a81e4ee9e9ae6f798c20a3ceba118fe
adrien <adrien.charrier@outlook.fr>
2023-11-08 17:41:24 +0100

    FEAT - Btn styles

--- resources/views/profile/partials/logout-user-form.blade.php
+++ resources/views/profile/partials/logout-user-form.blade.php
@@ -1,7 +1,7 @@
 <form method="POST" action="{{ route('logout') }}">
     @csrf
     <x-responsive-nav-link :href="route('logout')" class="btn"
-        onclick="event.preventDefault(); this.closest('form').submit();">
+        onclick="event.preventDefault(); this.closest('form').submit();" role="button">
         {{ __('Log Out') }}
     </x-responsive-nav-link>
 </form>
